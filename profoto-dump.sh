29614c638e0687a7cb1da5c2afc8217b22cbe2f0
Zoltan P <zoltan_padrah@users.sourceforge.net>
2012-09-16 16:02:51 +0200

    initial dump script

--- profoto-dump.sh
+++ profoto-dump.sh
@@ -6,5 +6,26 @@ set -e
 # to know what the script is doing
 set -x
 
+# settings of this script
+
+PXVIEW_VERSION=0.2.5
+
+# verify command line arguments
+if [ -z "$1"  -o  "$2"  ] ;
+	then
+	echo "Usage: $0 location_of_profoto_cd"
+	echo "exiting"
+	exit 1
+fi
+
+# dump the database
+"./work/pxview-$PXVIEW_VERSION/src/pxview \
+	$1/profoto/Profoto/profoto.db \
+	-b $1/profoto/Profoto/profoto.MB \
+	-p img \
+	--blobextension=bmp \
+	-x \
+	-o index.htm"
+
 # TODO
 echo TODO
